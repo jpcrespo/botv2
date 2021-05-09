60f6bbd3d85544b80996fc537be2a5c3469e1786
jpcrespo <jp.crespo.vargas@gmail.com>
2021-05-08 23:59:42 -0400

    test ind1

--- core/work.sh
+++ core/work.sh
@@ -2,7 +2,7 @@
 
 sudo supervisorctl stop telegrambot
 
-cd /home/pi/Desktop/telebot/covidbotbolivia/core/
+cd /home/pi/Desktop/telebot/botv2covidbotbolivia/core/
 
 sudo rm -r vacunas covid19-bolivia pics
 sudo rm estados.npy fechas.npy
@@ -14,7 +14,7 @@ cd ..
 cd ..
 source bin/activate
 
-cd /home/pi/Desktop/telebot/covidbotbolivia/core/
+cd /home/pi/Desktop/telebot/botv2/core/
 python recopilador.py
 python notif.py
 
@@ -23,8 +23,9 @@ git add vacunas/datos/primera.csv vacunas/datos/segunda.csv
 git commit -m 'actualizando datos de la fuente'
 git push origin master
 
-sudo rm -r covid19-bolivia vacunas __pycache__ ip.txt
+sudo rm -r covid19-bolivia vacunas 
+sudo rm ip.txt
+sudo rm -r __pycache__
 ifconfig > ip.txt
 sudo supervisorctl start telegrambot
-
 
