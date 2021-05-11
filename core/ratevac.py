a35da7ec96d392292768fd5b372883c6daa04400
rpiCrespo <jp.crespo.vargas@gmail.com>
2021-05-11 01:32:08 -0400

    rpi

--- core/ratevac.py
+++ core/ratevac.py
@@ -58,9 +58,6 @@ prop = fm.FontProperties(fname=fpath)
 fname = os.path.split(fpath)[1]
 
 
-bol = mpimg.imread('bol.jpg')
-imagebox = OffsetImage(bol,zoom=2)
-
 # These are the "Tableau 20" colors as RGB.    
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
              (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
