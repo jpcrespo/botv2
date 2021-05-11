a73840f84c5c0ce586c8a00249961c58fd4b196b
jpcrespo <jp.crespo.vargas@gmail.com>
2021-05-11 00:47:29 -0400

    graf epide

--- core/casos.py
+++ core/casos.py
@@ -5,6 +5,7 @@ import numpy as np
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)      
 import matplotlib.pyplot as plt
 
+_n=115.131 
 #datos casos diarios
 df3=pd.read_csv('covid19-bolivia/confirmados.csv',sep=',').sort_values(by='Fecha').set_index('Fecha')
 df4=pd.read_csv('covid19-bolivia/decesos.csv',sep=',').sort_values(by='Fecha').set_index('Fecha')
@@ -13,6 +14,7 @@ df4=pd.read_csv('covid19-bolivia/decesos.csv',sep=',').sort_values(by='Fecha').s
 #ambas concentraciones de datos tienen distintos etiquetados.
 #se replican los ordenes en distintos vectores.
 dep_c=['La Paz','Cochabamba','Santa Cruz','Oruro','Potosí','Tarija','Chuquisaca','Beni','Pando']
+c_dep = [26.99,17.53,26.48,4.93,8.21,4.81,5.76,4.20,8.21]
 
 data3 = df3.iloc[:,:].values.T
 data4 = df4.iloc[:,:].values.T
@@ -21,32 +23,29 @@ var_c=np.zeros((9,len(data3[0])))  #creamos una nueva variable
 var_m=np.zeros((9,len(data4[0])))  #iniciada en 0
 
 for j in range(9):
-    var_c[j,0]=data3[j,0]  #El primer dato es igual a los nuevos casos
-    var_m[j,0]=data4[j,0]  #dando el inicio del acumulado
+    var_c[j,0]=data3[j,0]/((_n/100)*c_dep[j])  #El primer dato es igual a los nuevos casos
+    var_m[j,0]=data4[j,0]/((_n/100)*c_dep[j])  #dando el inicio del acumulado
     for i in range(1,len(data3[0])):
-        var_c[j,i]=data3[j,i]-data3[j,i-1]
-        var_m[j,i]=data4[j,i]-data4[j,i-1]
+        var_c[j,i]=(data3[j,i]-data3[j,i-1])/((_n/100)*c_dep[j])
+        var_m[j,i]=(data4[j,i]-data4[j,i-1])/((_n/100)*c_dep[j])
 
 y_c=df3.index.values       #con el indice dado por las fechas del reporte
 
 mean_df_c = df3.rolling(7,min_periods=1).mean()
 mean_df_m = df4.rolling(7,min_periods=1).mean()
 
-
-mean_dc = mean_df_c.iloc[:,:].values.T   
-mean_dm = mean_df_m.iloc[:,:].values.T   
-
+mean_dc = mean_df_c.iloc[:,:].values.T  
+mean_dm = mean_df_m.iloc[:,:].values.T 
 
 var_mc=np.zeros((9,len(mean_dc[0])))
 var_mm=np.zeros((9,len(mean_dm[0])))
 
-
 for j in range(9):
-    var_mc[j,0]=mean_dc[j,0]
-    var_mm[j,0]=mean_dc[j,0]
+    var_mc[j,0]=(mean_dc[j,0]/((_n/100)*c_dep[j]))
+    var_mm[j,0]=(mean_dc[j,0]/((_n/100)*c_dep[j]))
     for i in range(1,len(data3[0])):
-        var_mc[j,i]=mean_dc[j,i]-mean_dc[j,i-1]
-        var_mm[j,i]=mean_dm[j,i]-mean_dm[j,i-1]
+        var_mc[j,i]=(mean_dc[j,i]-mean_dc[j,i-1])/((_n/100)*c_dep[j])
+        var_mm[j,i]=(mean_dm[j,i]-mean_dm[j,i-1])/((_n/100)*c_dep[j])
 
 plt.style.use('dark_background')
 
@@ -57,10 +56,6 @@ from matplotlib import font_manager as fm, rcParams
 fpath = os.path.join(r'MonoLisaSimpson-Regular.ttf')
 prop = fm.FontProperties(fname=fpath)
 fname = os.path.split(fpath)[1]
-
-
-bol = mpimg.imread('bol.jpg')
-imagebox = OffsetImage(bol,zoom=2)
 
 # These are the "Tableau 20" colors as RGB.    
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
@@ -74,62 +69,27 @@ for i in range(len(tableau20)):
     r, g, b = tableau20[i]    
     tableau20[i] = (r / 255., g / 255., b / 255.)   
 
-
-
 for i in range(len(dep_c)):
-    plt.figure(figsize=(45,38))
-    plt.title('NUEVOS CASOS/DÍA EN EL DEPARTAMENTO: '+dep_c[i]+'\n(último reporte en fuente: '+y_c[-1]+')\n',fontsize=80,fontproperties=prop)
-    plt.plot(y_c,var_c[i],label='Nuevos Casos/día',linewidth=5,color=tableau20[0],linestyle='-',marker='.', markersize=7,markeredgecolor='red',markerfacecolor='r')
+    plt.figure(figsize=(90,60))
+    plt.title('NUEVOS CASOS/DÍA POR 100\'000 HAB EN EL DEPARTAMENTO: '+dep_c[i]+'\n(último reporte en fuente: '+y_c[-1]+')\n',fontsize=150,fontproperties=prop)
+    plt.plot(y_c,var_c[i],label='Nuevos Casos/día',linewidth=5,color=tableau20[0],linestyle='-',marker='.', markersize=15,markeredgecolor='red',markerfacecolor='r')
     plt.plot(y_c,var_mc[i],label='Promedio 7 días',linewidth=15,color=tableau20[1],linestyle='-')
     plt.plot(y_c,var_m[i],label='Fallecimientos/día',linewidth=15,color=tableau20[7],linestyle='-')
-    plt.legend(loc='upper left',fontsize=50)
-    plt.yticks(fontsize=65,fontproperties=prop)
-    plt.xticks(y_c[::28],fontsize=55,rotation=45,fontproperties=prop)
-    plt.ylim(0,np.max(var_c[i]))  
-    plt.ylabel('Casos/día',fontsize=55,fontproperties=prop)
-    plt.gca().yaxis.grid(linestyle='--',linewidth=0.5,dashes=(5,15))
+    plt.legend(loc='upper left',fontsize=80)
+    plt.yticks(fontsize=100,fontproperties=prop)
+    plt.xticks(y_c[::28],fontsize=80,rotation=45,fontproperties=prop)
+    plt.ylim(0,2*np.max(var_mc[i]))  
+    plt.ylabel('Casos/día',fontsize=85,fontproperties=prop)
+    plt.gca().yaxis.grid(linestyle='--',linewidth=3,dashes=(5,15))
     plt.gca().spines["top"].set_visible(False)    
     plt.gca().spines["bottom"].set_visible(True)    
     plt.gca().spines["right"].set_visible(False)    
     plt.gca().spines["left"].set_visible(False)  
     plt.gca().get_xaxis().tick_bottom()    
     plt.gca().get_yaxis().tick_left()
-    plt.text(1,np.max(var_c[i])/1.3,"Data source: https://github.com/mauforonda/covid19-bolivia"    
+    plt.text(0,-8,"Data source: https://github.com/mauforonda/covid19-bolivia"    
        "\nAutor: Telegram Bot: @Bolivian_Bot"    
        "\nNota: Histórico acumulado",fontsize=30,fontproperties=prop);
     plt.savefig('pics/cov'+dep_c[i]+'.png')
     plt.close()
 
-# nacional1_=var_c[0]+var_c[1]+var_c[2]+var_c[3]+var_c[4]+var_c[5]+var_c[6]+var_c[7]+var_c[8]
-# nacional2_=var_mc[0]+var_mc[1]+var_mc[2]+var_mc[3]+var_mc[4]+var_mc[5]+var_mc[6]+var_mc[7]+var_mc[8]
-# nacional3_=var_mm[0]+var_mm[1]+var_mm[2]+var_mm[3]+var_mm[4]+var_mm[5]+var_mm[6]+var_mm[7]+var_mm[8]
-
-
-# plt.figure(figsize=(45,38))
-# plt.title('NUEVOS CASOS/DÍA A NIVEL NACIONAL'+'\n(último reporte en fuente: '+y_c[-1]+')\n',fontsize=80,fontproperties=prop)
-# plt.plot(y_c,nacional1_,label='Nuevos Casos/día',linewidth=5,color=tableau20[0],linestyle='-',marker='.',markersize=7,markeredgecolor='red',markerfacecolor='r')
-# plt.plot(y_c,nacional2_,label='Promedio 7 días',linewidth=15,color=tableau20[1],linestyle='-')
-# plt.plot(y_c,nacional3_,label='Fallecimientos/día',linewidth=15,color=tableau20[7],linestyle='-')
-
-# bol1 = mpimg.imread('bol.jpg')
-# imagebox1 = OffsetImage(bol1,zoom=2)
-# firma1 = AnnotationBbox(imagebox1,(60,np.max(nacional1_)/1.5))
-
-# plt.legend(loc='upper left',fontsize=50)
-# plt.yticks(fontsize=65,fontproperties=prop)
-# plt.xticks(y_c[::28],fontsize=55,rotation=45,fontproperties=prop)
-# plt.ylim(0,np.max(nacional1_))  
-# plt.ylabel('Casos/día',fontsize=55,fontproperties=prop)
-# plt.gca().yaxis.grid(linestyle='--',linewidth=0.5,dashes=(5,15))
-# plt.gca().spines["top"].set_visible(False)    
-# plt.gca().spines["bottom"].set_visible(True)    
-# plt.gca().spines["right"].set_visible(False)    
-# plt.gca().spines["left"].set_visible(False)  
-# plt.gca().get_xaxis().tick_bottom()    
-# plt.gca().get_yaxis().tick_left()
-# plt.gca().add_artist(firma1)
-# plt.text(1,1800,"Data source: https://github.com/mauforonda/covid19-bolivia"    
-#        "\nAutor: Telegram Bot: @Bolivian_Bot"    
-#        "\nNota: Histórico acumulado",fontsize=30,fontproperties=prop); 
-# plt.savefig('pics/covNac.png')
-# plt.close()
--- core/casosnac.py
+++ core/casosnac.py
@@ -1,10 +1,11 @@
-
 import os,sys
 import pandas as pd
 import numpy as np
-np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)      
+    
 import matplotlib.pyplot as plt
 
+
+_n=115.131 # factor de correción para llevar a 10^5 hab
 #datos casos diarios
 df3=pd.read_csv('covid19-bolivia/confirmados.csv',sep=',').sort_values(by='Fecha').set_index('Fecha')
 df4=pd.read_csv('covid19-bolivia/decesos.csv',sep=',').sort_values(by='Fecha').set_index('Fecha')
@@ -14,8 +15,8 @@ df4=pd.read_csv('covid19-bolivia/decesos.csv',sep=',').sort_values(by='Fecha').s
 #se replican los ordenes en distintos vectores.
 dep_c=['La Paz','Cochabamba','Santa Cruz','Oruro','Potosí','Tarija','Chuquisaca','Beni','Pando']
 
-data3 = df3.iloc[:,:].values.T
-data4 = df4.iloc[:,:].values.T
+data3 = (df3.iloc[:,:].values.T)/_n
+data4 = (df4.iloc[:,:].values.T)/_n
 
 var_c=np.zeros((9,len(data3[0])))  #creamos una nueva variable 
 var_m=np.zeros((9,len(data4[0])))  #iniciada en 0
@@ -33,8 +34,8 @@ mean_df_c = df3.rolling(7,min_periods=1).mean()
 mean_df_m = df4.rolling(7,min_periods=1).mean()
 
 
-mean_dc = mean_df_c.iloc[:,:].values.T   
-mean_dm = mean_df_m.iloc[:,:].values.T   
+mean_dc = (mean_df_c.iloc[:,:].values.T)/_n  
+mean_dm = (mean_df_m.iloc[:,:].values.T)/_n 
 
 
 var_mc=np.zeros((9,len(mean_dc[0])))
@@ -60,7 +61,8 @@ fname = os.path.split(fpath)[1]
 
 
 bol = mpimg.imread('bol.jpg')
-imagebox = OffsetImage(bol,zoom=2)
+imagebox = OffsetImage(bol,zoom=3)
+firma = AnnotationBbox(imagebox,(50,18))
 
 # These are the "Tableau 20" colors as RGB.    
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
@@ -75,61 +77,31 @@ for i in range(len(tableau20)):
     tableau20[i] = (r / 255., g / 255., b / 255.)   
 
 
-
-# for i in range(len(dep_c)):
-#     plt.figure(figsize=(45,38))
-#     plt.title('NUEVOS CASOS/DÍA EN EL DEPARTAMENTO: '+dep_c[i]+'\n(último reporte en fuente: '+y_c[-1]+')\n',fontsize=80,fontproperties=prop)
-#     plt.plot(y_c,var_c[i],label='Nuevos Casos/día',linewidth=5,color=tableau20[0],linestyle='-',marker='.', markersize=7,markeredgecolor='red',markerfacecolor='r')
-#     plt.plot(y_c,var_mc[i],label='Promedio 7 días',linewidth=15,color=tableau20[1],linestyle='-')
-#     plt.plot(y_c,var_m[i],label='Fallecimientos/día',linewidth=15,color=tableau20[7],linestyle='-')
-#     plt.legend(loc='upper left',fontsize=50)
-#     plt.yticks(fontsize=65,fontproperties=prop)
-#     plt.xticks(y_c[::28],fontsize=55,rotation=45,fontproperties=prop)
-#     plt.ylim(0,np.max(var_c[i]))  
-#     plt.ylabel('Casos/día',fontsize=55,fontproperties=prop)
-#     plt.gca().yaxis.grid(linestyle='--',linewidth=0.5,dashes=(5,15))
-#     plt.gca().spines["top"].set_visible(False)    
-#     plt.gca().spines["bottom"].set_visible(True)    
-#     plt.gca().spines["right"].set_visible(False)    
-#     plt.gca().spines["left"].set_visible(False)  
-#     plt.gca().get_xaxis().tick_bottom()    
-#     plt.gca().get_yaxis().tick_left()
-#     plt.text(1,np.max(var_c[i])/1.3,"Data source: https://github.com/mauforonda/covid19-bolivia"    
-#        "\nAutor: Telegram Bot: @Bolivian_Bot"    
-#        "\nNota: Histórico acumulado",fontsize=30,fontproperties=prop);
-#     plt.savefig('pics/cov'+dep_c[i]+'.png')
-#     plt.close()
-
 nacional1_=var_c[0]+var_c[1]+var_c[2]+var_c[3]+var_c[4]+var_c[5]+var_c[6]+var_c[7]+var_c[8]
 nacional2_=var_mc[0]+var_mc[1]+var_mc[2]+var_mc[3]+var_mc[4]+var_mc[5]+var_mc[6]+var_mc[7]+var_mc[8]
 nacional3_=var_mm[0]+var_mm[1]+var_mm[2]+var_mm[3]+var_mm[4]+var_mm[5]+var_mm[6]+var_mm[7]+var_mm[8]
 
 
-plt.figure(figsize=(45,38))
-plt.title('NUEVOS CASOS/DÍA A NIVEL NACIONAL'+'\n(último reporte en fuente: '+y_c[-1]+')\n',fontsize=80,fontproperties=prop)
-plt.plot(y_c,nacional1_,label='Nuevos Casos/día',linewidth=5,color=tableau20[0],linestyle='-',marker='.',markersize=7,markeredgecolor='red',markerfacecolor='r')
+plt.figure(figsize=(90,60))
+plt.title('\nNUEVOS CASOS/DÍA POR 100\'000 HAB A NIVEL NACIONAL'+'\n(último reporte en fuente: '+y_c[-1]+')\n',fontsize=150,fontproperties=prop)
+plt.plot(y_c,nacional1_,label='Nuevos Casos/día',linewidth=5,color=tableau20[0],linestyle='-',marker='.',markersize=15,markeredgecolor='red',markerfacecolor='r')
 plt.plot(y_c,nacional2_,label='Promedio 7 días',linewidth=15,color=tableau20[1],linestyle='-')
 plt.plot(y_c,nacional3_,label='Fallecimientos/día',linewidth=15,color=tableau20[7],linestyle='-')
-
-bol1 = mpimg.imread('bol.jpg')
-imagebox1 = OffsetImage(bol1,zoom=2)
-firma1 = AnnotationBbox(imagebox1,(60,np.max(nacional1_)/1.5))
-
-plt.legend(loc='upper left',fontsize=50)
-plt.yticks(fontsize=65,fontproperties=prop)
-plt.xticks(y_c[::28],fontsize=55,rotation=45,fontproperties=prop)
-plt.ylim(0,np.max(nacional1_))  
-plt.ylabel('Casos/día',fontsize=55,fontproperties=prop)
-plt.gca().yaxis.grid(linestyle='--',linewidth=0.5,dashes=(5,15))
+plt.legend(loc='upper left',fontsize=80)
+plt.yticks(fontsize=100,fontproperties=prop)
+plt.xticks(y_c[::28],fontsize=80,rotation=45,fontproperties=prop)
+plt.ylim(0,np.max(nacional2_)+5)  
+plt.ylabel('Casos/día',fontsize=85,fontproperties=prop)
+plt.gca().yaxis.grid(linestyle='--',linewidth=3,dashes=(5,15))
 plt.gca().spines["top"].set_visible(False)    
 plt.gca().spines["bottom"].set_visible(True)    
 plt.gca().spines["right"].set_visible(False)    
 plt.gca().spines["left"].set_visible(False)  
 plt.gca().get_xaxis().tick_bottom()    
 plt.gca().get_yaxis().tick_left()
-plt.gca().add_artist(firma1)
-plt.text(1,1800,"Data source: https://github.com/mauforonda/covid19-bolivia"    
+plt.gca().add_artist(firma)
+plt.text(0,-8,"Data source: https://github.com/mauforonda/covid19-bolivia"    
        "\nAutor: Telegram Bot: @Bolivian_Bot"    
-       "\nNota: Histórico acumulado",fontsize=30,fontproperties=prop); 
+       "\nNota: Histórico acumulado",fontsize=50,fontproperties=prop); 
 plt.savefig('pics/covNac.png')
-plt.close()
+
